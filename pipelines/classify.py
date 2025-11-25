1b6a627b7e038a15d43f789d5738f2bfab33ec0d
Uriel Pineda <upinedaser@gmail.com>
2025-11-25 15:36:02 -0600

    remove key

--- pipelines/classify.py
+++ pipelines/classify.py
@@ -4,7 +4,7 @@ from google import genai
 
 MODEL_ID = "gemini-2.5-flash"
 
-client = genai.Client(api_key="AIzaSyA8bbtuWVDDVIcx2zVtIr3A1jaPvJUHvZ0")
+client = genai.Client(api_key="Modify_with_your_GenAI_API_Key")
 
 SECTORS = [
     "minero-energetico",
